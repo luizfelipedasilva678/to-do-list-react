24ce8d684b040e8ff65c45f4fce58895a17df534
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-06-20 17:18:57 -0300

    Eslint and general configs

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+    'import/first': 'off',
+    'no-param-reassign': 'off',
+    'react/jsx-filename-extension': 'off',
+  },
+};
